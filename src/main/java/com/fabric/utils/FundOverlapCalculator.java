15343f7a96ae6e83fd683cc2c5c6ae9c6a9511a9
Manishy-97 <manishy@thoughtworks.com>
2023-04-16 22:11:45 +0530

    add an overlap calculator util

---
+++ src/main/java/com/fabric/utils/FundOverlapCalculator.java
@@ -0,0 +1,21 @@
+package com.fabric.utils;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FundOverlapCalculator {
+    public String calculateOverlap(Set<String> firstStock, Set<String> secondStock) {
+        int totalStocksCount = firstStock.size() + secondStock.size();
+        ArrayList unionStockList = new ArrayList<String>();
+        unionStockList.addAll(firstStock);
+        unionStockList.addAll(secondStock);
+        HashSet uniqueStockList = new HashSet<String>(unionStockList);
+        int commonStockCount = totalStocksCount - uniqueStockList.size();
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setMinimumFractionDigits(2);
+        double overlap = (200 * (double) commonStockCount / totalStocksCount);
+        return df.format(overlap);
+    }
+}
