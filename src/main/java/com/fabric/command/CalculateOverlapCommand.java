fe151d71c11d3befb0d9b15b702d6df3f60663f2
Manishy-97 <manishy@thoughtworks.com>
2023-04-17 08:40:45 +0530

    add CalculateOverlapCommand

---
+++ src/main/java/com/fabric/command/CalculateOverlapCommand.java
@@ -0,0 +1,36 @@
+package com.fabric.command;
+
+import com.fabric.DataStore;
+import com.fabric.PortFolio;
+import com.fabric.utils.FundOverlapCalculator;
+
+import java.util.Set;
+
+public class CalculateOverlapCommand implements Command {
+    private PortFolio portFolio;
+    private DataStore dataStore;
+
+    public CalculateOverlapCommand(PortFolio portFolio, DataStore dataStore) {
+        this.portFolio = portFolio;
+        this.dataStore = dataStore;
+    }
+
+    public void Execute(String command) throws Exception {
+        String fund = command.split(" ")[1];
+        Set<String> requestedFundStocks = dataStore.getStocksFor(fund);
+        for (String currentFund : this.portFolio.getFunds()) {
+            try {
+                Set<String> currentFundStock = dataStore.getStocksFor(currentFund);
+                String overlapPercent = new FundOverlapCalculator().calculate(currentFundStock, requestedFundStocks);
+                if (Double.parseDouble(overlapPercent) > (double) 0) {
+                    System.out.println(fund + " " + currentFund + " " + overlapPercent + "%");
+                }
+            } catch (Exception exception) {
+                if (("FUND_NOT_FOUND").equals(exception.getMessage())) {
+                    System.out.println(exception.getMessage());
+                }
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/fabric/commands/CalculateOverlapCommandTest.java
@@ -0,0 +1,45 @@
+package com.fabric.commands;
+
+import com.fabric.DummyDataStore;
+import com.fabric.PortFolio;
+import com.fabric.command.CalculateOverlapCommand;
+import com.fabric.command.CurrentPortfolioCommand;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculateOverlapCommandTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void shouldCalculateFundOverlapWithKnownFund() throws Exception {
+        DummyDataStore dataStore = new DummyDataStore();
+        PortFolio portFolio = new PortFolio();
+        String currentPortfolioInstruction = "CURRENT_PORTFOLIO AXIS_BLUECHIP ICICI_PRU_BLUECHIP UTI_NIFTY_INDEX";
+        CurrentPortfolioCommand currentPortfolioCommand = new CurrentPortfolioCommand(portFolio);
+        currentPortfolioCommand.Execute(currentPortfolioInstruction);
+        String calculateOverlapInstruction = "CALCULATE_OVERLAP MIRAE_ASSET_LARGE_CAP";
+        CalculateOverlapCommand calculateOverlapCommand = new CalculateOverlapCommand(portFolio, dataStore);
+        calculateOverlapCommand.Execute(calculateOverlapInstruction);
+        assertEquals("MIRAE_ASSET_LARGE_CAP AXIS_BLUECHIP 40.00%\n" +
+                "MIRAE_ASSET_LARGE_CAP ICICI_PRU_BLUECHIP 33.33%\n" +
+                "MIRAE_ASSET_LARGE_CAP UTI_NIFTY_INDEX 40.00%\n", outputStreamCaptor.toString());
+    }
+}
