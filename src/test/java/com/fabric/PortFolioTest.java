af00dd927dc54b39749e404b7548de72eebc6268
Manishy-97 <manishy@thoughtworks.com>
2023-04-17 08:35:19 +0530

    add portfolio to store user portfolio

---
+++ src/test/java/com/fabric/PortFolioTest.java
@@ -0,0 +1,18 @@
+package com.fabric;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PortFolioTest {
+    @Test
+    void shouldAddAndGetFunds() {
+        PortFolio portFolio = new PortFolio();
+        portFolio.addFunds(Arrays.asList("FUND1", "FUND2"));
+        assertEquals(portFolio.getFunds(), Arrays.asList("FUND1", "FUND2"));
+    }
+}
+
+
