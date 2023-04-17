329e7f2aca0ac4b3f0c1f3289c262c7e2f95debd
Manishy-97 <manishy@thoughtworks.com>
2023-04-17 10:25:52 +0530

    add AddStockCommand to add a stock for a given fund

---
+++ src/main/java/com/fabric/command/AddStockCommand.java
@@ -0,0 +1,25 @@
+package com.fabric.command;
+
+import com.fabric.DataStore;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class AddStockCommand implements Command {
+    private DataStore dataStore;
+
+    public AddStockCommand(DataStore dataStore) {
+        this.dataStore = dataStore;
+    }
+
+    @Override
+    public void Execute(String command) throws Exception {
+        String[] args = command.split(" ");
+        String[] instruction = Arrays.copyOfRange(command.split(" "), 1, args.length);
+        String fundName = instruction[0];
+        String stockName = String.join(" ", Arrays.copyOfRange(instruction, 1, instruction.length));
+        Set<String> currentFundStocks = dataStore.getStocksFor(fundName);
+        currentFundStocks.add(stockName);
+    }
+}
---
+++ src/test/java/com/fabric/commands/AddStockCommandTest.java
@@ -0,0 +1,27 @@
+package com.fabric.commands;
+
+import com.fabric.DummyDataStore;
+import com.fabric.PortFolio;
+import com.fabric.command.AddStockCommand;
+import com.fabric.command.CurrentPortfolioCommand;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AddStockCommandTest {
+    @Test
+    void execute() throws Exception {
+        String command = "ADD_STOCK AXIS_BLUECHIP HDFC BANK LIMITED";
+        DummyDataStore dataStore = new DummyDataStore();
+        Set<String> axisBlueChipStocksBeforeUpdate = dataStore.getStocksFor("AXIS_BLUECHIP");
+        assertEquals(Arrays.asList("INFOSYS LIMITED", "BHARTI AIRTEL LIMITED"), axisBlueChipStocksBeforeUpdate.stream().collect(Collectors.toList()));
+        AddStockCommand addStockCommand = new AddStockCommand(dataStore);
+        addStockCommand.Execute(command);
+        Set<String> axisBlueChipStocks = dataStore.getStocksFor("AXIS_BLUECHIP");
+        assertEquals(Arrays.asList("INFOSYS LIMITED", "BHARTI AIRTEL LIMITED", "HDFC BANK LIMITED"), axisBlueChipStocks.stream().collect(Collectors.toList()));
+    }
+}
