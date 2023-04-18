3a0d872f249eb9fb16ff34dd1b6cea9976db4a38
Manishy-97 <manishy@thoughtworks.com>
2023-04-18 09:43:06 +0530

    remove unused method

--- src/main/java/com/fabric/models/FundDataStore.java
+++ src/main/java/com/fabric/models/FundDataStore.java
@@ -4,12 +4,7 @@ import java.util.ArrayList;
 
 public class FundDataStore {
     private ArrayList<MutualFund> funds;
-
     public ArrayList<MutualFund> getFunds() {
         return funds;
-    }
-
-    public void setFunds(ArrayList<MutualFund> funds) {
-        this.funds = funds;
     }
 }
