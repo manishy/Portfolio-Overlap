468eec795550cde6c32da6cf03c45e7037516155
Manishy-97 <manishy@thoughtworks.com>
2023-04-18 06:29:21 +0530

    update main to print output based on cli args

--- src/main/java/com/fabric/Main.java
+++ src/main/java/com/fabric/Main.java
@@ -1,7 +1,21 @@
 package com.fabric;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dataSourceFileName = "stock_data.json";
+        try {
+            Path inputFilePath = Paths.get(args[0]);
+            MutualFundDataStore mutualFundDataStore = new MutualFundDataStore(dataSourceFileName);
+            CommandExecuter commandExecuter = new CommandExecuter(mutualFundDataStore, new PortFolio());
+            List<String> instructions = Files.readAllLines(inputFilePath);
+            for (String instruction : instructions) commandExecuter.execute(instruction);
+        } catch (Exception e) {
+            System.out.println("SOMETHING_WENT_WRONG");
+        }
     }
 }
