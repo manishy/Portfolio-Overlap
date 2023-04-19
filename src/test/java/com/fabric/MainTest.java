1bd175131e8da7b2bde6548e2405c2df1aad231c
manishy <manishyadav12354@gmail.com>
2023-04-19 05:37:42 +0530

    add test coverage for main class

---
+++ src/test/java/com/fabric/MainTest.java
@@ -0,0 +1,41 @@
+package com.fabric;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void mainShouldExecuteCommandsProvidedFromFile() {
+        new Main().main(new String[]{"src/test/resources/test_input.txt"});
+        assertEquals("FUND_NOT_FOUND\n" +
+                "FUND_NOT_FOUND\n", outputStreamCaptor.toString());
+    }
+
+    @Test
+    void mainShouldPrintErrorIfAnyExceptionWasThrown() {
+        String testInputPath = "src/test/resources/invalid_file.txt";
+        new Main().main(new String[]{testInputPath});
+        assertEquals("SOMETHING_WENT_WRONG\n", outputStreamCaptor.toString());
+    }
+}
