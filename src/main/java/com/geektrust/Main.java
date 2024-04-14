67eca199428854f62383059fc80cafab57def7d0
manishy <manishyadav12354@gmail.com>
2024-04-14 11:52:41 +0530

    [Manish]: fix issue with the portfolio

--- src/main/java/com/geektrust/Main.java
+++ src/main/java/com/geektrust/Main.java
@@ -12,8 +12,9 @@ public class Main {
             Path inputFilePath = Paths.get(args[0]);
             MutualFundDataStore mutualFundDataStore = new MutualFundDataStore(dataSourceFileName);
             CommandInvoker commandInvoker = new CommandInvoker(mutualFundDataStore, new Printer());
+            PortFolio portFolio = new PortFolio();
             List<String> instructions = Files.readAllLines(inputFilePath);
-            for (String instruction : instructions) commandInvoker.invoke(new PortFolio(), instruction);
+            for (String instruction : instructions) commandInvoker.invoke(portFolio, instruction);
         } catch (Exception e) {
             System.out.println("SOMETHING_WENT_WRONG");
         }
